d8564823dc213d3c0721c4f7a5ab7486b467f0c4
Eduard Shahnazaryan <squared1994@gmail.com>
2020-05-28 00:18:08 +0400

    Initial Commit

---
+++ QRLib/QRLib.h
@@ -0,0 +1,19 @@
+//
+//  QRLib.h
+//  QRLib
+//
+//  Created by Eduard Shahnazaryan on 5/28/20.
+//  Copyright © 2020 Eduard Shahnazaryan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRLib.
+FOUNDATION_EXPORT double QRLibVersionNumber;
+
+//! Project version string for QRLib.
+FOUNDATION_EXPORT const unsigned char QRLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRLib/PublicHeader.h>
+
+
