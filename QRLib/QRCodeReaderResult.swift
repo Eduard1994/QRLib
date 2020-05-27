8f5096f8e9014afd6b7d144ddb7f26040fdaa1ed
Eduard Shahnazaryan <squared1994@gmail.com>
2020-05-28 00:33:38 +0400

    First Commit

---
+++ QRLib/QRCodeReaderResult.swift
@@ -0,0 +1,25 @@
+//
+//  QRCodeReaderResult.swift
+//  QRScanner
+//
+//  Created by Eduard Shahnazaryan on 5/27/20.
+//  Copyright © 2020 Eduard Shahnazaryan. All rights reserved.
+//
+
+import Foundation
+
+/**
+ The result of the scan with its content value and the corresponding metadata type.
+ */
+public struct QRCodeReaderResult {
+    /**
+     The error corrected data decoded into a human-readable string.
+     */
+    public let value: String
+    
+    /**
+     The type of the metadata.
+     */
+    public let metadataType: String
+}
+
